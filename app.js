d0a0e99be3849acfe75606b5388f6c5da24f01ab
Soham Dhodapkar <dsoham298@gmail.com>
2019-11-14 01:07:12 -0500

    added cors

--- app.js
+++ app.js
@@ -36,16 +36,11 @@ app.use(bodyParser.json());
 
 //! COMMENTED OUT BECAUSE OF ACCESS PROBLEMS TO LOCALHOST
 //TODO: FIGURE OUT WHAT'S GOING WRONG
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header(
-//         "Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//         );
-//         if (req.method === 'OPTIONS') {
-//             res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-//             return res.status(200).json({});
-//         }
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 // Routes to handle requests
